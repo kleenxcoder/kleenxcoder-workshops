db1e129a45afbef9e719ba84b6cf7cbb233243ad
Peter Stritzinger <der.stritzinger@gmail.com>
2020-10-20 09:59:01 +0200

    add json example

---
+++ json/spring.rest.jackson/src/test/java/kleenxcoder/spring/rest/jackson/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kleenxcoder.spring.rest.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
